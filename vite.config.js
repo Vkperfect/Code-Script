98e63f7d7522ac08c11eef878bee31935b5ddcfe
Vikram <vikram.gupta.2513@gmail.com>
2024-04-13 17:08:29 +0530

    correct all config

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Code-Script/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./src/main",
-      },
-      output: {
-        format: "es",
-        entryFileNames: "[name].[hash].js",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  server: {
-    port: 3000,
-  },
 });
