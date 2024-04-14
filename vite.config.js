20e4f524c1276b8983117682ff0d77578d78218e
Vikram <vikram.gupta.2513@gmail.com>
2024-04-14 17:28:40 +0530

    config for host on render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Code-Script/",
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./src/main.jsx",
+      },
+    },
+  },
 });
